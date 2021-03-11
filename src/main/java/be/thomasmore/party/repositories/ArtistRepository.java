61c02972383f9d592e35426ea495aed4d450e76e
simenraes <simieraes@hotmail.com>
2021-03-11 13:14:17 +0100

    bundel 3

--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -1,7 +1,21 @@
 package be.thomasmore.party.repositories;
 
 import be.thomasmore.party.model.Artist;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ArtistRepository extends CrudRepository<Artist, Integer> {
+    List<Artist> findAllBy();
+    List<Artist> findByArtistNameContainingIgnoreCase(String keyword);
+
+    @Query("select a from Artist a WHERE " +
+            ":keyword IS NULL OR " +
+            "(UPPER(a.artistName) LIKE UPPER(CONCAT('%', :keyword, '%'))) OR " +
+            "(UPPER(a.bio) LIKE UPPER(CONCAT('%', :keyword, '%'))) OR " +
+            "(UPPER(a.genre) LIKE UPPER(CONCAT('%', :keyword, '%'))) OR " +
+            "(UPPER(a.portfolio) LIKE UPPER(CONCAT('%', :keyword, '%')))")
+    List<Artist> findByKeyword(@Param("keyword")String keyword);
 }
--- src/main/java/be/thomasmore/party/repositories/VenueRepository.java
+++ src/main/java/be/thomasmore/party/repositories/VenueRepository.java
@@ -1,11 +1,27 @@
 package be.thomasmore.party.repositories;
 
 import be.thomasmore.party.model.Venue;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface VenueRepository extends CrudRepository<Venue, Integer> {
-    Iterable<Venue> findByOutdoor(boolean isOutdoor);
-    Iterable<Venue> findByIndoor(boolean isIndoor);
-    Iterable<Venue> findByCapacityBetween(int min, int max);
-    Iterable<Venue> findByCapacityGreaterThan(int min);
+    @Query("SELECT v FROM Venue v WHERE " +
+            "(:minCapacity IS NULL OR :minCapacity <= v.capacity) AND " +
+            "(:maxCapacity IS NULL OR v.capacity <= :maxCapacity) AND " +
+            "(:maxDistance IS NULL OR v.distanceFromPublicTransportInKm <= :maxDistance) AND " +
+            "(:foodProvided IS NULL OR v.foodProvided = :foodProvided) AND " +
+            "(:indoor IS NULL OR v.indoor=:indoor) AND " +
+            "(:outdoor IS NULL OR v.outdoor=:outdoor) ")
+    List<Venue> findByFilter(@Param("minCapacity") Integer minCapacity,
+                             @Param("maxCapacity") Integer maxCapacity,
+                             @Param("maxDistance") Integer maxDistance,
+                             @Param("foodProvided") Boolean foodProvided,
+                             @Param("indoor") Boolean indoor,
+                             @Param("outdoor") Boolean outdoor);
+
+
+
 }
